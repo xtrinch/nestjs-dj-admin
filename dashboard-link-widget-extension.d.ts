585392d232b9f4720c7f2761a910590d5cfc9e34
Mojca <mojca.rojko@gmail.com>
2026-04-17 13:36:50 +0200

    add extension subpath declarations

---
+++ dashboard-link-widget-extension.d.ts
@@ -0,0 +1,2 @@
+export { dashboardLinkWidgetExtension } from './src/extensions/dashboard-link-widget/index.js';
+export type { DashboardLinkWidgetExtensionOptions } from './src/extensions/dashboard-link-widget/index.js';
